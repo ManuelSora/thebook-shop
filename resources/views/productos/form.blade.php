7ade0972c3d2e467421e597811bd34832e00c600
Manuel Nuñez <manuel_manh@outlook.com>
2020-06-11 03:49:50 -0700

    Version 3.5
    Testeando todas las vistas

--- resources/views/productos/form.blade.php
+++ resources/views/productos/form.blade.php
@@ -1,45 +1,45 @@
 <div class="form-group">
-    <label for="Titulo" class="control-label">{{'Titulo'}}</label>
-    <input class="form-control {{ $errors->has('Titulo')?'is-invalid':'' }}" type="text" name="Titulo" id="Titulo" value="{{ old('Titulo') }}">
-    {!! $errors->first('Titulo','<div class="invalid-feedback">:message</div>') !!}
+    <label for="titulo" class="control-label">{{'titulo'}}</label>
+    <input class="form-control {{ $errors->has('titulo')?'is-invalid':'' }}" type="text" name="titulo" id="titulo" value="{{ old('titulo') }}">
+    {!! $errors->first('titulo','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-    <label for="Genero" class="form-label">{{'Genero'}}</label>
-    <input class="form-control {{ $errors->has('Genero')?'is-invalid':'' }}" type="text" name="Genero" id="Genero" value="{{ old('Genero') }}">
-    {!! $errors->first('Genero','<div class="invalid-feedback">:message</div>') !!}
+    <label for="genero" class="form-label">{{'genero'}}</label>
+    <input class="form-control {{ $errors->has('Gengeneroero')?'is-invalid':'' }}" type="text" name="genero" id="genero" value="{{ old('genero') }}">
+    {!! $errors->first('genero','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-    <label for="Descripcion" class="form-label">{{'Descripcion:'}}</label>
-    <input class="form-control {{ $errors->has('Descripcion')?'is-invalid':'' }}" type="text" name="Descripcion" id="Descripcion"
-        value="{{ old('Descripcion') }}">
-    {!! $errors->first('Descripcion','<div class="invalid-feedback">:message</div>') !!}
-</div>
-
-
-<div class="form-group">
-    <label for="Precio" class="form-label">{{'Precio'}}</label>
-    <input class="form-control {{ $errors->has('Precio')?'is-invalid':'' }}" type="text" name="Precio" id="Precio" value="{{ old('Precio') }}">
-    {!! $errors->first('Precio','<div class="invalid-feedback">:message</div>') !!}
+    <label for="descripcion" class="form-label">{{'descripcion:'}}</label>
+    <input class="form-control {{ $errors->has('descripcion')?'is-invalid':'' }}" type="text" name="descripcion" id="descripcion"
+        value="{{ old('descripcion') }}">
+    {!! $errors->first('descripcion','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 
 <div class="form-group">
-    <label for="Autor" class="form-label">{{'Autor'}}</label>
-    <input class="form-control {{ $errors->has('Autor')?'is-invalid':'' }}" type="text" name="Autor" id="Autor" value="{{ old('Autor') }}">
-    {!! $errors->first('Autor','<div class="invalid-feedback">:message</div>') !!}
+    <label for="precio" class="form-label">{{'precio'}}</label>
+    <input class="form-control {{ $errors->has('precio')?'is-invalid':'' }}" type="text" name="precio" id="precio" value="{{ old('precio') }}">
+    {!! $errors->first('precio','<div class="invalid-feedback">:message</div>') !!}
+</div>
+
+
+<div class="form-group">
+    <label for="autor" class="form-label">{{'autor'}}</label>
+    <input class="form-control {{ $errors->has('autor')?'is-invalid':'' }}" type="text" name="autor" id="autor" value="{{ old('autor') }}">
+    {!! $errors->first('autor','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-    <label for="Portada" class="form-label">{{'Portada'}}</label>
+    <label for="portada" class="form-label">{{'portada'}}</label>
     @if (isset($producto->portada))
     <br />
-    <img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$producto->portada}}" alt="" width="200">
+    <img class="img-fluid" src="{{ asset('storage').'/'.$producto->portada}}" alt="" width="200">
     <br />
     @endif
 
-    <input class="form-control {{ $errors->has('Portada')?'is-invalid':'' }}" type="file" name="Portada" id="Portada"
+    <input class="form-control {{ $errors->has('portada')?'is-invalid':'' }}" type="file" name="portada" id="portada"
         value="">
 </div>
 
