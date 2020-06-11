055a7cafbd51cfcd9af595f92612ebb7de510d2b
Manuel Nuñez <manuel_manh@outlook.com>
2020-06-11 03:42:32 -0700

    Version 3.5
    Testeando todas las vistas

--- resources/views/empleados/form.blade.php
+++ resources/views/empleados/form.blade.php
@@ -1,46 +1,37 @@
-
-
-
-
 <div class="form-group">
-<label for="NombreEmpleado" class="control-label" >{{'Nombre'}}</label>
-<input type="text" class="form-control {{ $errors->has('NombreEmpleado')?'is-invalid':'' }}" name="NombreEmpleado" id="NombreEmpleado" 
-value="{{ isset($empleado->NombreEmpleado)?$empleado->NombreEmpleado:old('NombreEmpleado') }}">
-{!! $errors->first('NombreEmpleado','<div class="invalid-feedback">:message</div>') !!}
+    <label for="NombreEmpleado" class="control-label">{{'Nombre'}}</label>
+    <input class="form-control {{ $errors->has('NombreEmpleado')?'is-invalid':'' }}" type="text" name="NombreEmpleado" id="NombreEmpleado" value="{{ old('NombreEmpleado') }}">
+    {!! $errors->first('NombreEmpleado','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-<label for="ApellidoEmpleado" class="form-label" >{{'Apellido'}}</label>
-<input type="text" class="form-control {{ $errors->has('ApellidoEmpleado')?'is-invalid':'' }}" name="ApellidoEmpleado" id="ApellidoEmpleado" 
-value="{{ isset($empleado->ApellidoEmpleado)?$empleado->ApellidoEmpleado:old('ApellidoEmpleado') }}">
-{!! $errors->first('ApellidoEmpleado','<div class="invalid-feedback">:message</div>') !!}
+    <label for="ApellidoEmpleado" class="form-label">{{'Apellido'}}</label>
+    <input class="form-control {{ $errors->has('ApellidoEmpleado')?'is-invalid':'' }}" type="text" name="ApellidoEmpleado" id="ApellidoEmpleado" value="{{ old('ApellidoEmpleado') }}">
+    {!! $errors->first('ApellidoEmpleado','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-<label for="Edad" class="form-label" >{{'Edad'}}</label>
-<input type="text" class="form-control {{ $errors->has('Edad')?'is-invalid':'' }}" name="Edad" id="Edad" 
-value="{{ isset($empleado->Edad)?$empleado->Edad:old('Edad') }}">
-{!! $errors->first('Edad','<div class="invalid-feedback">:message</div>') !!}
+    <label for="Edad" class="form-label">{{'Edad'}}</label>
+    <input class="form-control {{ $errors->has('Edad')?'is-invalid':'' }}" type="text" name="Edad" id="Edad" value="{{ old('Edad') }}">
+    {!! $errors->first('Edad','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-<label for="Correo" class="form-label" >{{'Correo'}}</label>
-<input type="email" class="form-control {{ $errors->has('Correo')?'is-invalid':'' }}" name="Correo" id="Correo" 
-value="{{ isset($empleado->Correo)?$empleado->Correo:old('Correo') }}">
-{!! $errors->first('Correo','<div class="invalid-feedback">:message</div>') !!}
+    <label for="Correo" class="form-label">{{'Correo'}}</label>
+    <input class="form-control {{ $errors->has('Correo')?'is-invalid':'' }}" type="email" name="Correo" id="Correo" value="{{ old('Correo') }}">
+    {!! $errors->first('Correo','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-<label for="Foto" class="form-label" >{{'Foto'}}</label>
-@if (isset($empleado->Foto))
-<br/>
-<img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$empleado->Foto}}" alt="" width="200">
-<br/>
-@endif
+    <label for="Foto" class="form-label">{{'Foto'}}</label>
+    @if (isset($empleado->Foto))
+    <br />
+    <img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$empleado->Foto}}" alt="" width="200">
+    <br />
+    @endif
 
-<input class="form-control {{ $errors->has('Foto')?'is-invalid':'' }}" type="file" name="Foto" id="Foto" value="">
+    <input class="form-control {{ $errors->has('Foto')?'is-invalid':'' }}" type="file" name="Foto" id="Foto" value="">
 </div>
 
-<input type="submit" class="btn btn-success btn-outline-success"
-value="{{ $Modo=='crear' ? 'Agregar':'Modificar' }}">
-<a class="btn btn-primary btn-outline-primary" href="{{ url('empleados') }}" >Regresar</a>
+<input type="submit" class="btn btn-success btn-outline-success" value="{{ $Modo=='crear' ? 'Agregar':'Modificar' }}">
+<a class="btn btn-primary btn-outline-primary" href="{{ url('empleados') }}">Regresar</a>
--- resources/views/productos/form.blade.php
+++ resources/views/productos/form.blade.php
@@ -1,51 +1,47 @@
-
-
 <div class="form-group">
-<label for="Titulo" class="control-label" >{{'Titulo'}}</label>
-<input type="text" class="form-control {{ $errors->has('Titulo')?'is-invalid':'' }}" name="Titulo" id="Titulo"
-value="{{ isset($producto->titulo)?$producto->titulo:old('titulo') }}">
-{!! $errors->first('Titulo','<div class="invalid-feedback">:message</div>') !!}
+    <label for="Titulo" class="control-label">{{'Titulo'}}</label>
+    <input class="form-control {{ $errors->has('Titulo')?'is-invalid':'' }}" type="text" name="Titulo" id="Titulo" value="{{ old('Titulo') }}">
+    {!! $errors->first('Titulo','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-<label for="Genero" class="form-label" >{{'Genero'}}</label>
-<input type="text" class="form-control {{ $errors->has('Genero')?'is-invalid':'' }}" name="Genero" id="Genero"
-value="{{ isset($producto->genero)?$producto->genero:old('genero') }}">
-{!! $errors->first('Genero','<div class="invalid-feedback">:message</div>') !!}
+    <label for="Genero" class="form-label">{{'Genero'}}</label>
+    <input class="form-control {{ $errors->has('Genero')?'is-invalid':'' }}" type="text" name="Genero" id="Genero" value="{{ old('Genero') }}">
+    {!! $errors->first('Genero','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-<label for="Precio" class="form-label" >{{'Precio'}}</label>
-<input type="text" class="form-control {{ $errors->has('Precio')?'is-invalid':'' }}" name="Precio" id="Precio"
-value="{{ isset($producto->precio)?$producto->precio:old('precio') }}">
-{!! $errors->first('Precio','<div class="invalid-feedback">:message</div>') !!}
+    <label for="Descripcion" class="form-label">{{'Descripcion:'}}</label>
+    <input class="form-control {{ $errors->has('Descripcion')?'is-invalid':'' }}" type="text" name="Descripcion" id="Descripcion"
+        value="{{ old('Descripcion') }}">
+    {!! $errors->first('Descripcion','<div class="invalid-feedback">:message</div>') !!}
+</div>
+
+
+<div class="form-group">
+    <label for="Precio" class="form-label">{{'Precio'}}</label>
+    <input class="form-control {{ $errors->has('Precio')?'is-invalid':'' }}" type="text" name="Precio" id="Precio" value="{{ old('Precio') }}">
+    {!! $errors->first('Precio','<div class="invalid-feedback">:message</div>') !!}
+</div>
+
+
+<div class="form-group">
+    <label for="Autor" class="form-label">{{'Autor'}}</label>
+    <input class="form-control {{ $errors->has('Autor')?'is-invalid':'' }}" type="text" name="Autor" id="Autor" value="{{ old('Autor') }}">
+    {!! $errors->first('Autor','<div class="invalid-feedback">:message</div>') !!}
 </div>
 
 <div class="form-group">
-<label for="Descripcion" class="form-label" >{{'Email:'}}</label>
-<input type="email" class="form-control {{ $errors->has('Descripcion')?'is-invalid':'' }}" name="Descripcion" id="Descripcion"
-value="{{ isset($producto->descripcion)?$producto->descripcion:old('descripcion') }}">
-{!! $errors->first('Descripcion','<div class="invalid-feedback">:message</div>') !!}
+    <label for="Portada" class="form-label">{{'Portada'}}</label>
+    @if (isset($producto->portada))
+    <br />
+    <img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$producto->portada}}" alt="" width="200">
+    <br />
+    @endif
+
+    <input class="form-control {{ $errors->has('Portada')?'is-invalid':'' }}" type="file" name="Portada" id="Portada"
+        value="">
 </div>
 
-<div class="form-group">
-<label for="Autor" class="form-label" >{{'Autor'}}</label>
-<input type="text" class="form-control {{ $errors->has('Autor')?'is-invalid':'' }}" name="Autor" id="Autor"
-value="{{ isset($producto->autor)?$producto->autor:old('autor') }}">
-{!! $errors->first('Autor','<div class="invalid-feedback">:message</div>') !!}
-</div>
-
-<div class="form-group">
-<label for="Portada" class="form-label" >{{'Portada'}}</label>
-@if (isset($producto->portada))
-<br/>
-<img class="img-thumbnail img-fluid" src="{{ asset('storage').'/'.$producto->portada}}" alt="" width="200">
-<br/>
-@endif
-
-<input class="form-control {{ $errors->has('Portada')?'is-invalid':'' }}" type="file" name="Portada" id="Portada" value="">
-</div>
-
-<input type="submit" class="btn btn-outline-success"
-value="{{ $Modo=='crear' ? 'Agregar':'Modificar' }}">
-<a class="btn btn-outline-primary" href="{{ url('productos') }}" >Regresar</a>
+<input type="submit" class="btn btn-outline-success" value="{{ $Modo=='crear' ? 'Agregar':'Modificar' }}">
+<a class="btn btn-outline-primary" href="{{ url('productos') }}">Regresar</a>
